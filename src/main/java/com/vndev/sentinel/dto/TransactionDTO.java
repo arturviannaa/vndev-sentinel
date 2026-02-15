cf6c0dd4a1838e6db06a500004e7e51ef72c47dc
arturviannaa <arturvianna@icloud.com>
2026-02-15 05:26:53 -0300

    contrato json e redis implementado

--- src/main/java/com/vndev/sentinel/dto/TransactionDTO.java
+++ src/main/java/com/vndev/sentinel/dto/TransactionDTO.java
@@ -1,4 +1,21 @@
 package com.vndev.sentinel.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransactionDTO {
+    private String cardToken;
+    private String userId;
+    private BigDecimal amount;
+    private String merchantCategory;
+    private Double latitude;
+    private Double longitude;
 }
